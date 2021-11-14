db9d1c1b2a09ca9dbf75ac8134577dc6b1b0769c
starwolf8 <18685018+starwolf8@users.noreply.github.com>
2021-11-13 21:59:50 -0800

    cleanup files

--- microservices/receipt-ms/main.go
+++ microservices/receipt-ms/main.go
@@ -5,7 +5,6 @@ import (
 	"receipt-ms/database"
 	"receipt-ms/receipt"
 
-	// "ms-demo/product"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -18,6 +17,5 @@ func main() {
 	log.Print("Initializing Receipts Microservice\n")
 	database.SetupDatabase()
 	receipt.SetupRoutes(apiBasePath)
-	// product.SetupRoutes(apiBasePath)
 	http.ListenAndServe(":5001", nil)
 }
